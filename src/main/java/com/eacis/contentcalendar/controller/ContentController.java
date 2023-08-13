d259832dd03a60e060a3457a54aff32245e0c0f9
Chamod Isuru <92633908+isuruisuru@users.noreply.github.com>
2023-08-13 15:26:49 +0530

    getmapping and postmapping

--- src/main/java/com/eacis/contentcalendar/controller/ContentController.java
+++ src/main/java/com/eacis/contentcalendar/controller/ContentController.java
@@ -1,9 +1,38 @@
 package com.eacis.contentcalendar.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.eacis.contentcalendar.model.Content;
+import com.eacis.contentcalendar.repository.ContentCollectionRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
+    private final ContentCollectionRepository repository;
+
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    public List<Content> findAll(){
+        return repository.findAll();
+    }
+
+    @ResponseStatus(HttpStatus.FOUND)
+    @GetMapping("/{id}")
+    public Content findById(@PathVariable Integer id){
+        return repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found!"));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public void create(@RequestBody Content content){
+        repository.save(content);
+    }
 }
--- src/main/java/com/eacis/contentcalendar/repository/ContentCollectionRepository.java
+++ src/main/java/com/eacis/contentcalendar/repository/ContentCollectionRepository.java
@@ -1,25 +1,40 @@
 package com.eacis.contentcalendar.repository;
 
 import com.eacis.contentcalendar.model.Content;
+import com.eacis.contentcalendar.model.Status;
+import com.eacis.contentcalendar.model.Type;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public class ContentCollectionRepository {
-    private final List<Content> content = new ArrayList<>();
+    private final List<Content> contentList = new ArrayList<>(
+            Arrays.asList(
+                    new Content(1,"My first blog post","My first blog post", Status.IDEA, Type.ARTICLE, LocalDateTime.now(),null,""),
+                    new Content(2,"My second blog post","My second blog post", Status.IN_PROGRESS, Type.VIDEO, LocalDateTime.now(),null,""),
+                    new Content(3,"My third blog post","My third blog post", Status.IDEA, Type.ARTICLE, LocalDateTime.now(),null,"")
+
+            )
+    );
 
     public ContentCollectionRepository() {
 
     }
 
     public List<Content> findAll() {
-        return content;
+        return contentList;
     }
 
     public Optional<Content> findById(Integer id){
-        return content.stream().filter(c -> c.id().equals(id)).findFirst();
+        return contentList.stream().filter(c -> c.id().equals(id)).findFirst();
+    }
+
+    public void save(Content content) {
+        contentList.add(content);
     }
 }
